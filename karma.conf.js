17f08e7e17d384b299fe63f238b0f8d231948f50
Yuchen He <yuchen@runoranj.com>
2017-07-06 12:12:29 -0500

    add: add karma dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,13 @@ module.exports = function(config) {
         './bower_components/angular/angular.js',
         './bower_components/angular-mocks/angular-mocks.js',
         './bower_components/angular-ui-router/release/angular-ui-router.js',
-        './bower_components/oranj-navigation/navigation.directive.js',
+        './bower_components/angular-material/angular-material.js',
+        './bower_components/angular-animate/angular-animate.js',
+        './bower_components/angular-aria/angular-aria.js',
+        './bower_components/angular-sanitize/angular-sanitize.js',
+        './bower_components/angular-messages/angular-messages.js',
+
+        './bower_components/oranj-navigation/bower_dist/main.js',
         './bower_components/highcharts/highcharts.js',
         './bower_components/oranj-highcharts/oranjHighcharts.js',
         //module (this should be before your app)

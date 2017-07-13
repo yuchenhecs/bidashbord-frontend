86f230aecea07774c9b2c3c7a310b13e751d12c6
Robert Yuan <robertyuan@Roberts-MacBook-Pro.local>
2017-07-13 17:11:56 -0500

    finished: leaderboard landing page

--- src/app/leaderBoard/leaderBoard.controller.js
+++ src/app/leaderBoard/leaderBoard.controller.js
@@ -14,4 +14,30 @@ function LeaderBoardController($scope, $mdDialog, LeaderBoardService, LeaderBoar
     this.showChart = function (ev) {
         LeaderBoardDialogService.showChart(ev);
     };
+
+    $scope.highlightButton = function (scope) {
+        $scope.lbOverall = 'offFocus';
+        $scope.lbState = 'offFocus';
+        $scope.lbFirm = 'offFocus';
+
+        if (scope === 'overall') {
+            $scope.lbOverall = 'onFocus';
+            $scope.scope = scope;
+        } else if (scope === 'state') {
+            $scope.lbState = 'onFocus';
+            $scope.scope = scope;
+        } else {
+            $scope.lbFirm = 'onFocus';
+            $scope.scope = scope;
+        }
+    };
+
+    $scope.changeScope = function (scope) {
+        //this.apiCallFunctionToBeWritten
+        $scope.highlightButton(scope);
+    };
+
+    //have the default scope set to state
+    $scope.changeScope('state');
+
 }

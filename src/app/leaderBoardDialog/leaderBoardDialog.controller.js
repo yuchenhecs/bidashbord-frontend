d6379c6874c087bcd58e2b498536dcaf1a1f73a6
Yuchen He <yuchen@runoranj.com>
2017-07-19 12:27:30 -0500

    change: popup dialog layout changed, include data for overall, state and firm

--- src/app/leaderBoardDialog/leaderBoardDialog.controller.js
+++ src/app/leaderBoardDialog/leaderBoardDialog.controller.js
@@ -97,53 +97,99 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
 
 
     this.showChart = function (ev) {
+
+        var service = this.init();
+
+        console.log("asd");
         var dialogHTML = `
-            <md-dialog style="width:720px">
-                <div>
+            <md-dialog style="width:720px;overflow: visible">
+                <div style="position: relative">
+                    <div id="tag" class="md-whiteframe-2dp" style="visibility: hidden">
+                        <h4 style="margin:0">Asset Under Management</h4>
+                    </div>
+
+
                     <div layout="column" >
-                        <div layout="row" layout-align="end stretch" style="padding-top:5px; padding-right:5px">
-                            <a> Overall </a> | <a> State </a> | <a> Firm </a>
-                        </div>
-                        <div layout="row" layout-align="space-around stretch">
-                            <h4>title</h4>
+                        <div layout="row" layout-align="end center"  layout-padding>
+                            <div>
+                                <h2 style="margin:0">777 <small>k</small></h2>
+                            </div>
                         </div>
                         
-                        <div layout="row" layout-align="space-around center">
-                         <div flex="25" style="text-align: center;margin:auto">
-                                <h1>777 <small>k</small></h1>
+                        <div layout="row" layout-align="space-between center"  layout-padding>
+
+                            <div flex="15" style="text-align: center;margin:auto">
+                                <h4>Overall</h4>
                             </div>
-                                <div style="height:100px;border-left: thin solid #dfdfdf;">
+
+                            <div style="height:100px;border-right: thin solid #dfdfdf;">
                             </div>
-                            
+                         
                             <div flex="25" style="text-align: center">
-                                <div id="chart-sm" style="height:150px"></div>
-                                <h6 style="margin:0">least in state</h6>
+                                <div id="chart-overall" style="height:120px"></div>
                             </div>
 
-                            <div flex="25" style="text-align: center;margin:auto">
+                            <div flex="30" style="text-align: center;margin:auto">
                                 <h1>777 <small>k</small></h1>
                                 <h3>least in state</h3>
                             </div>
 
-                            <div flex="25" style="text-align: center;margin:auto">
+                            <div flex="30" style="text-align: center;margin:auto">
                                 <h1>777 <small>k</small></h1>
                                 <h3>least in state</h3>
                             </div>
                         </div>
-                            <div layout="row" layout-align="center center" style="margin:0">
-                                <div flex="80" id="chart-lg"  style="max-height:300px">
-                                    <div class="loader no-animate primary-loader loader--style3">
-                                    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                        width="40px" height="40px" viewBox="0 0 50 50" style="enable-background: new 0 0 50 50;" xml:space="preserve">
-                                        <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-                                            <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"
-                                            />
-                                        </path>
-                                    </svg>
-                                </div>
+
+
+                        <div layout="row" layout-align="space-around center"  layout-padding>
+                            <div flex="15" style="text-align: center;margin:auto">
+                                <h4>State</h4>
                             </div>
+
+                            <div style="height:100px;border-right: thin solid #dfdfdf;">
+                            </div>
+                         
+                            <div flex="25" style="text-align: center">
+                                <div id="chart-state" style="height:120px"></div>
+                            </div>
+
+                            <div flex="30" style="text-align: center;margin:auto">
+                                <h1>777 <small>k</small></h1>
+                                <h3>least in state</h3>
+                            </div>
+
+                            <div flex="30" style="text-align: center;margin:auto">
+                                <h1>777 <small>k</small></h1>
+                                <h3>least in state</h3>
+                            </div>
+                        </div>
+
+                      
+
+
+                        <div layout="row" layout-align="space-around center"  layout-padding>
+                            <div flex="15" style="text-align: center;margin:auto">
+                                <h4>Firm</h4>
+                            </div>
+
+                            <div style="height:100px;border-right: thin solid #dfdfdf;">
+                            </div>
+                         
+                            <div flex="25" style="text-align: center">
+                                <div id="chart-firm" style="height:120px"></div>
+                            </div>
+
+                            <div flex="30" style="text-align: center;margin:auto">
+                                <h1>777 <small>k</small></h1>
+                                <h3>least in state</h3>
+                            </div>
+
+                            <div flex="30" style="text-align: center;margin:auto">
+                                <h1>777 <small>k</small></h1>
+                                <h3>least in state</h3>
+                            </div>
+                        </div>
                             
-                        </div>
 
                         <md-tabs md-stretch-tabs="always" md-no-pagination="'true'" md-no-ink="'true'" md-no-ink-bar="'true'" style="visibility: hidden;"> 
                             <md-tab label="&nbsp;&nbsp; AUM &nbsp;&nbsp;"></md-tab>
@@ -162,17 +208,21 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
             </md-dialog>
         `;
 
-        var service = this.init();
-
-
+        
         $mdDialog.show({
-            controller: LeaderBoardDialogController,
+            controller: LeaderBoardController,
             template: dialogHTML,
             parent: angular.element(document.getElementById('main-container')),
             targetEvent: ev,
             clickOutsideToClose: true,
             onComplete: () => {
                 document.getElementsByTagName("md-tabs")[0].style['visibility'] = "";
+
+                document.getElementById("tag").style['visibility'] = "";
+                
+
+                document.getElementById("tag").classList.add("tag-one");
+                
                 document.querySelector("md-tab-item:nth-child(1)").classList.add("tab-one");
                 document.querySelector("md-tab-item:nth-child(2)").classList.add("tab-one");
                 document.querySelector("md-tab-item:nth-child(3)").classList.add("tab-one");
@@ -187,26 +237,9 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
                 document.querySelector("md-tab-item:nth-child(10)").classList.add("tab-three");
 
 
-
-                //  var rangeBlocks = text.element.children;
-
-                // for (var i = 0; i < rangeBlocks.length; i = i + 2) {
-                //     rangeBlocks[i].setAttribute('data-isWeek', i);
-                //     rangeBlocks[i].classList.add("path-link");
-                //     rangeBlocks[i].classList.add("chart-legend");
-                //     rangeBlocks[i].onclick = function () {
-                //         base.rangeOnClick(this);
-                //     };
-                // };
-
-                // var tmp = base.isWeek ? 0 : 2;
-                // rangeBlocks[tmp].classList.add("curr-path-link");
-
-
-                LeaderBoardDialogService.self.createRingChart();
-                service.launch();
-                service.chart.reflow();
-
+                LeaderBoardDialogService.self.createRingChart('chart-overall');
+                LeaderBoardDialogService.self.createRingChart('chart-state');
+                LeaderBoardDialogService.self.createRingChart('chart-firm');
             }
         });
 
@@ -219,13 +252,14 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
 
 
 
-    this.createRingChart = function () {
-        var chart_sm = Highcharts.chart('chart-sm', {
+    this.createRingChart = function (id) {
+        var chart_sm = Highcharts.chart(id, {
             credits: {
                 enabled: false
             },
             chart: {
-                type: 'solidgauge'
+                type: 'solidgauge',
+                margin: [0, 0, 0, 0]
             },
             title: {
                 text: null
@@ -237,8 +271,8 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
                 startAngle: 0,
                 endAngle: 360,
                 background: [{ // Track for Move
-                    outerRadius: '112%',
-                    innerRadius: '88%',
+                    outerRadius: '115%',
+                    innerRadius: '100%',
                     backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0])
                         .setOpacity(0.3)
                         .get(),
@@ -261,7 +295,7 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
                         useHTML: true,
                         shadow: false,
                         style: {
-                            fontSize: '16px'
+                            fontSize: '12px'
                         },
                         formatter: function () {
                             return '<div style="width:100%;text-align:center;"><span style="font-size:1.2em;font-weight:bold;">' + this.point.series.name + '</span><br/><span style="font-size:3em;color:' + Highcharts.getOptions().colors[0] + ';font-weight:bold;">' + Highcharts.numberFormat(this.y / 10, 0) + '</span>';
@@ -276,8 +310,8 @@ function LeaderBoardDialogService(MetricsService, $mdDialog) {
                 name: 'Move',
                 data: [{
                     color: Highcharts.getOptions().colors[0],
-                    radius: '112%',
-                    innerRadius: '88%',
+                    radius: '115%',
+                    innerRadius: '100%',
                     y: 80
                 }]
             }]
